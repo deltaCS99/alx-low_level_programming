57e66c4a8e447d073294051964909d1379144994
deltaCS99 <kambangacraig@gmail.com>
2022-12-09 12:26:05 -0800

    fgets

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fputs("with proper grammar, but the outcome is a piece of art,\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
