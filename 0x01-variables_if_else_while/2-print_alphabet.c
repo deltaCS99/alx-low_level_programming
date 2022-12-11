8bbb0a0db02140a54e0082abafb9783648166530
deltaCS99 <kambangacraig@gmail.com>
2022-12-11 13:39:28 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	char a;
+
+	for ( a = 'a' ; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+
+	return (0);
+}
