40fdc4aab4d9b02dbf1117fa412addf52690e0fb
deltaCS99 <kambangacraig@gmail.com>
2022-12-11 13:56:35 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print-alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z'; a++)
+	{
+		if (a != 'q' || a != 'e')
+		{
+			putchar(a);
+
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
