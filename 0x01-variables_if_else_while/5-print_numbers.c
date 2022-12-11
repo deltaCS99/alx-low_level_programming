796dfba4dd278b7b2af176bcb01cc8b50da40344
deltaCS99 <kambangacraig@gmail.com>
2022-12-11 14:18:01 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0' ; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
