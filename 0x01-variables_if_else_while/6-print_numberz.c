7a3600cb9901e89ccddd032f0cdf902e9588273c
deltaCS99 <kambangacraig@gmail.com>
2022-12-11 14:30:48 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0' ; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
