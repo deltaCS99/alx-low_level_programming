648269b476ad3895c7822f9f35c7dd07a69e6b91
deltaCS99 <kambangacraig@gmail.com>
2022-12-11 22:31:22 -0800

    comb2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; a <= 8 ; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+
+			if (a != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
