662d13ef584c8cba87b211f4f47372092ea21108
deltaCS99 <kambangacraig@gmail.com>
2022-12-13 10:47:42 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check lowercase
+ * Return: 1 if lower, 0 if otherwise
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	return (0);
+
+}
