e088b6701d83a97bd41092d265c507d7454a46c8
deltaCS99 <kambangacraig@gmail.com>
2022-12-16 06:43:25 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - is digit
+ * @c: number
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+
+	return (0);
+}
