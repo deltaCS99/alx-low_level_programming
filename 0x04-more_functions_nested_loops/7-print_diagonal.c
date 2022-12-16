f7d2d15f6084fd05b851431e1525526e3b870905
deltaCS99 <kambangacraig@gmail.com>
2022-12-16 13:46:47 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,18 +17,8 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n + 1; j++)
-			{
-				if (i == j)
-				{
-					_putchar('\\');
-				}
-				else
-				{
-					_putchar(' ');
-				}
-			}
-			_putchar('\n');
+			_putchar(92);
 		}
+		_putchar('\n');
 	}
 }
