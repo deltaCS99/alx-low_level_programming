81d173697cd97e848ab1c09d3a9ff8982dbfc55e
Tapiwa <kambangacraig@gmail.com>
2022-12-19 23:47:54 +0200

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+ 
+ void print_rev(char *s)
+ {
+     int len = strlen(s);
+     int i;
+     char reversed[len];
+     
+     for(i = 0 ; i < len; i++)
+     {
+        reversed[len-i-1] = s[i];
+     }
+    
+     printf("%s\n",reversed);
+ }
