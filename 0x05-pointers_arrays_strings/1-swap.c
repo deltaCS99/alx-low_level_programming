115b41c190d450d799a63a46efe6a40460f7c1c4
Tapiwa <kambangacraig@gmail.com>
2022-12-19 23:12:57 +0200

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -12,14 +12,3 @@
      *a = *b;
      *b = temp;
  }
-int main() {
-    int a;
-    int b;
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
-}
