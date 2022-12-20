cccb20b2bd0b0993f993647764bd8d33e47f30a9
deltaCS99 <kambangacraig@gmail.com>
2022-12-20 15:18:21 -0800

    str length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strlen - check the code
+ * @s: string
+ * Return: string length.
  */
- int _strlen(char *s)
- {
-    return strlen(s);
- }
+int _strlen(char *s)
+{
+	int i;
+	char *c = s;
+
+	i = 0;
+
+	while (*c != '\0')
+	{
+		i++;
+		c++;
+	}
+
+	return (i);
+}
