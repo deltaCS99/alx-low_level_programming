8783e0ef1fb0116f45f213afc782f6d55f4ecd4b
deltaCS99 <kambangacraig@gmail.com>
2022-12-20 14:59:21 -0800

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * swap_int - check the code
+ * @a: first int
+ * @b: second int
+ * Return: void.
  */
- 
- void swap_int(int *a, int *b)
- {
-     int temp = *a;
-     *a = *b;
-     *b = temp;
- }
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
