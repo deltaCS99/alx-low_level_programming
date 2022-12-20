14d164bca7bc91f35b0d1bcbb86cab6fc8f123ef
deltaCS99 <kambangacraig@gmail.com>
2022-12-20 14:29:47 -0800

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * reset_to_98 - check the code
  *@n : number
  *
  * Return: Always 0.
- */ 
+ */
 void reset_to_98(int *n)
 {
-    *n = 98;    
- }
+	*n = 98;
+}
