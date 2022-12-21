cb7136b407f0b2e4658ce123a091dbd64357d662
deltaCS99 <kambangacraig@gmail.com>
2022-12-20 16:00:39 -0800

    print-rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - check the code
+ * print_rev - check the code
  *
- * Return: Always 0.
+ * @s: string
+ *
+ * Return: void
  */
- 
- void print_rev(char *s)
- {
-     int len = strlen(s);
-     int i;
-     char* reversed =malloc(len + 1);
-     
-     for(i = 0 ; i < len; i++)
-     {
-        reversed[len-i-1] = s[i];
-     }
-    
-     printf("%s\n",reversed);
- }
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+	char *c = &s[len - 1];
+
+	while (*c != '\0')
+	{
+		_putchar(*c);
+		c--;
+	}
+	_putchar('\n');
+}
