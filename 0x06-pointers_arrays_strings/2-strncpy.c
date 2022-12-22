4af44bebab83c33ff7b7c186fe5f4f5c79d1ec26
deltaCS99 <kambangacraig@gmail.com>
2022-12-22 08:00:27 -0800

    str n copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - check the code
+ * @dest: string
+ * @src: string
+ * @n: n
+ *
+ * Return: string copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
