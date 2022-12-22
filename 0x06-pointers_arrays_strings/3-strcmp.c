4871b8c25395cd6a83338751096423a1e3bccbba
deltaCS99 <kambangacraig@gmail.com>
2022-12-22 08:06:25 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - check the code
+ * @s1: string
+ * @s2: string
+ *
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
