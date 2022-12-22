81138a805516cafa33494d1737f16b0398fabefd
deltaCS99 <kambangacraig@gmail.com>
2022-12-22 07:52:30 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - check the code
+ * @dest: dest
+ * @src: src
+ * @n: n
+ * Return: pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
