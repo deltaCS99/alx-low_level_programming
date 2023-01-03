dc1401f0dec694d94612e107bdbb02a3ba6c7141
deltaCS99 <kambangacraig@gmail.com>
2023-01-03 14:48:52 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+/**
+ * _strchr -  locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return:  first occurance of char c or null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		i++;
+	}
+	return (NULL);
+}
